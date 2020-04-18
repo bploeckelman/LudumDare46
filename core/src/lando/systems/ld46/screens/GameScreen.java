5a500e50f11e2fd504ecdad307801b329900a6bf
Brian <brossman@kiio.com>
2020-04-18 12:20:40 -0500

    game screen

---
+++ core/src/lando/systems/ld46/screens/GameScreen.java
@@ -0,0 +1,15 @@
+package lando.systems.ld46.screens;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import lando.systems.ld46.Game;
+
+public class GameScreen extends BaseScreen {
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+}
