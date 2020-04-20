0581b3e5d768cc62dba1cd0ee843cd90bcf9c405
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2020-04-19 19:01:37 -0500

    Remove old test level and tilesets

--- core/assets/maps/tileset01_prototype_basicTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="tileset01_prototype_basicTiles" tilewidth="32" tileheight="32" tilecount="40" columns="4">
- <image source="tileset01_prototype_basicTiles.png" width="128" height="320"/>
-</tileset>
