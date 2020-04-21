522b56b31e169567e90b0424534d6237068c1864
Doug Graham <dsgraham@gmail.com>
2020-04-20 20:37:23 -0500

    New title screen

--- core/src/lando/systems/ld46/screens/TitleScreen.java
+++ core/src/lando/systems/ld46/screens/TitleScreen.java
@@ -26,30 +26,10 @@ public class TitleScreen extends BaseScreen {
     @Override
     public void update(float dt) {
         super.update(dt);
-        placeholder.add(vel.x * dt, vel.y * dt);
-        if (placeholder.x < 0) {
-            placeholder.x = 0;
-            vel.x *= -1;
-        }
-        if (placeholder.y < 0) {
-            placeholder.y = 0;
-            vel.y *= -1;
-        }
-        if (placeholder.x > Config.windowWidth - size) {
-            placeholder.x = Config.windowWidth - size;
-            vel.x *= -1;
-        }
-        if (placeholder.y > Config.windowHeight - size) {
-            placeholder.y = Config.windowHeight - size;
-            vel.y *= -1;
-        }
 
         if (Gdx.input.justTouched() || Gdx.input.isKeyJustPressed(Input.Keys.ENTER)){
             game.setScreen(new GameScreen(game), assets.cubeShader, 3f);
         }
-//        if (Gdx.input.isKeyJustPressed(Input.Keys.P)){
-//            game.setScreen(new EndScreen(game), assets.cubeShader, 3f);
-//        }
     }
 
     @Override
@@ -58,7 +38,6 @@ public class TitleScreen extends BaseScreen {
         batch.setProjectionMatrix(shaker.getCombinedMatrix());
         batch.setColor(Color.WHITE);
         batch.draw(assets.titleImage, 0,0, worldCamera.viewportWidth, worldCamera.viewportHeight);
-        batch.draw(assets.debugTexture, placeholder.x, placeholder.y, size, size);
 
         batch.end();
     }
