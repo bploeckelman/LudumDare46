a4571a26289f0624d184a1ba6859e006b17d52b3
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2020-04-20 19:49:07 -0500

    Add bullshit smaller level that sucks because nio can't handle the awesomer level

--- core/src/lando/systems/ld46/world/LevelDescriptor.java
+++ core/src/lando/systems/ld46/world/LevelDescriptor.java
@@ -4,6 +4,7 @@ public enum LevelDescriptor {
 
       test("maps/test.tmx")
     , level1("maps/level-1.tmx")
+    , level_main("maps/level-main.tmx")
     , level_tutorial("maps/level-tutorial.tmx")
     , level_boss("maps/level-arena.tmx")
     ;
